136b4dd8e7f244844792f98adbd71af8c13ad432
devriazul <engr.riazul@gmail.com>
2025-08-18 05:32:13 +0600

    Data type added

---
+++ datatype.py
@@ -0,0 +1,46 @@
+# Data Types 
+
+# 1. String
+name = "Riazul"
+print(name)
+print(type(name))
+
+# 2. Integer
+age = 20
+print(age)
+print(type(age))
+
+# 3. Float
+pi = 3.14
+print(pi)
+print(type(pi))
+
+# 4. Boolean
+is_student = True
+print(is_student)
+print(type(is_student))
+
+# 5. List
+fruits = ["apple", "banana", "orange"]
+print(fruits)
+print(type(fruits))
+
+# 6. Tuple
+coordinates = (10, 20)
+print(coordinates)
+print(type(coordinates))
+
+# 7. Set
+colors = {"red", "green", "blue"}
+print(colors)
+print(type(colors))
+
+# 8. Dictionary
+person = {"name": "Riazul", "age": 20}
+print(person)
+print(type(person))
+
+# 9. None
+value = None
+print(value)
+print(type(value))
