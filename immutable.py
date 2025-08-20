8261f019f93d9dbe41b3ac61463b039f7f099e41
devriazul <engr.riazul@gmail.com>
2025-08-20 08:20:33 +0600

    Mutable data types

--- immutable.py
+++ immutable.py
@@ -9,3 +9,8 @@ print(first_location)
 a = 6
 second_location = id(a)
 print(second_location)
+
+print(first_location == second_location)
+
+# Output:
+# True
---
+++ mutable.py
@@ -0,0 +1,10 @@
+# Mutable data types
+# List, Dictionary, Set
+
+# Example:
+
+my_list = [1, 2, 3]
+print(id(my_list))
+
+my_list.append(4)
+print(id(my_list))
