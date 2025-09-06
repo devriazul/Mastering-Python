b48e771e5fb7c003b57dff82405411e3a70bb584
devriazul <engr.riazul@gmail.com>
2025-09-06 07:28:22 +0600

    String

---
+++ string.py
@@ -0,0 +1,18 @@
+#single quote 
+name = 'Riazul'
+print(name)
+#double quote
+name = "Riazul"
+print(name)
+#triple quote
+name = """Riazul"""
+print(name)
+#triple single quote
+name = '''Riazul'''
+print(name)
+# console program
+name = input("Enter your name: ")
+print("Hello " + name)
+# Output:
+# Enter your name: Riazul
+# Hello Riazul
